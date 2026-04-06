afa8a696a31357a79345882b03b17ed8a8c87d96
shankar212 <shankerr7780@gmail.com>
2026-04-06 09:45:14 +0530

    Update phone number to raw official value

--- lib/content.ts
+++ lib/content.ts
@@ -201,7 +201,7 @@ export const testimonials = [
 ];
 
 export const contactInfo = {
-  phone: "+91 98765 43210",
+  phone: "+91 9440586832",
   address: "Gulab Singh Film City, Adilabad, Telangana, India",
   mapsEmbed:
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3759.6434907997286!2d78.48046217521753!3d19.556913581749253!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd267a5fb313fa5%3A0xdc7de073e3b107aa!2sGulab%20Singh%20Film%20city!5e0!3m2!1sen!2sin!4v1774943266812!5m2!1sen!2sin",
